70ea74f9125bb2a7298e41949a05329c1df96789
khalidh223 <khalidhussain4656@gmail.com>
2023-12-24 00:18:53 -0600

    Initial Commit

---
+++ fake-artist-ios/fake_artist_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  fake_artist_iosApp.swift
+//  fake-artist-ios
+//
+//  Created by Khalid Hussain on 12/24/23.
+//
+
+import SwiftUI
+
+@main
+struct fake_artist_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
